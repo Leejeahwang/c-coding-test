d32b19bc3a99970237e2c3a34086da0e4cc536a1
Leejeahwang <89955462+Leejeahwang@users.noreply.github.com>
2025-05-21 15:02:28 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250521/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250521/DateTime to DateTime/datetime-to-datetime.cpp
@@ -9,10 +9,10 @@ int main() {
 
     int end = c + b * 60 + a * 24 * 60;
 
-    int sum = end - (11 + 11 * 60 + 11 * 24 * 60);
+    int first = 11 + 11 * 60 + 11 * 24 * 60;
 
-    if(sum > 0) cout << sum;
-    else cout << -1;
+    if(end < first) cout << -1;
+    else cout << end - first;
 
     return 0;
 }
