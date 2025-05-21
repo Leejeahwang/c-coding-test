d121f09f6075f57338663781b9dbff4a42e82542
Leejeahwang <89955462+Leejeahwang@users.noreply.github.com>
2025-05-21 14:56:18 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250521/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250521/DateTime to DateTime/datetime-to-datetime.cpp
@@ -9,7 +9,10 @@ int main() {
 
     int end = c + b * 60 + a * 24 * 60;
 
-    cout << end - (11 + 11 * 60 + 11 * 24 * 60);
+    int sum = end - (11 + 11 * 60 + 11 * 24 * 60);
+
+    if(sum > 0) cout << sum;
+    else cout << -1;
 
     return 0;
 }
