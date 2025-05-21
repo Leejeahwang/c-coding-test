fe29e9e7633805c55f8acd439aed215a68c6e653
Leejeahwang <89955462+Leejeahwang@users.noreply.github.com>
2025-05-21 14:51:33 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250521/Date to Date/date-to-date.cpp
+++ 250521/Date to Date/date-to-date.cpp
@@ -7,24 +7,25 @@ int m1, d1, m2, d2;
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
 
+    int elapsed_days = 0;
+    
+    //                         1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
     int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if(m1 == m2){
-        cout << d2 - d1 + 1;
-        return 0;
+    
+    while(true) {
+        if(m1 == m2 && d1 == d2)
+            break;
+    
+        elapsed_days++;
+        d1++;
+    
+        if(d1 > num_of_days[m1]) {
+            m1++;
+            d1 = 1;
+        }
     }
-
-    int a = 0;
-    for(int i = 1; i <= m2; i++){
-        a += num_of_days[i];
-    }
-
-    int b = 0;
-    for(int i = 1; i <= m1; i++){
-        b += num_of_days[i];
-    }
-
-    cout << (a+d2) - (b+d1);
+    
+    cout << elapsed_days+1;
 
     return 0;
 }
